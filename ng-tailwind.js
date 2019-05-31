a2edfab4c353520dc942d8817b1ca8978b585ac5
DriverPortalDev <driverportaldev@gmail.com>
2019-05-31 08:55:56 +0000

    tailwind inital

---
+++ ng-tailwind.js
@@ -0,0 +1,23 @@
+module.exports = {
+  // Tailwind Paths
+  configJS: '/workspace/driverportalweb/tailwind.config.js',
+  sourceCSS: '/workspace/driverportalweb/src/tailwind.css',
+  outputCSS: '/workspace/driverportalweb/src/styles.css',
+  // Sass
+  sass: false,
+  // PurgeCSS Settings
+  purge: false,
+  keyframes: false,
+  fontFace: false,
+  rejected: false,
+  whitelist: [],
+  whitelistPatterns: [],
+  whitelistPatternsChildren: [],
+  extensions: [
+    '.ts',
+    '.html',
+    '.js'
+  ],
+  extractors: [],
+  content: []
+}
