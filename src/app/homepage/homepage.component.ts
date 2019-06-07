b3d053afced354e318e0dd95ef206d2e69d0edac
DriverPortalDev <driverportaldev@gmail.com>
2019-06-07 05:47:54 +0000

    text update

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
+  scrolling: boolean
 
   constructor() { }
 
   ngOnInit() {
+    if (typeof window !== undefined) {
+      window.addEventListener('scroll', () => {
+         this.checkScroll();
+      });
+    }
+  }
+
+  checkScroll() {
+    if (typeof window !== undefined) {
+      if (window.pageYOffset > 20) {
+        console.log('scrolled!');
+        this.scrolling = true;
+      } else if (window.pageYOffset < 20 && this.scrolling) {
+        console.log('unscrolled');
+      }
+    }
   }
 
 }
