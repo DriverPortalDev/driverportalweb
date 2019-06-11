4fa9cfa17318187745af9ca48e4b86c28960bc8d
DriverPortalDev <driverportaldev@gmail.com>
2019-06-11 05:40:02 +0000

    breakpoints update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   theme: {
+    screens: {
+      'phone': '360px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px'
+    },
     extend: {}
   },
   variants: {},
